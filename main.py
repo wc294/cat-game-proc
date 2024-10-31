733495fdb7c9ab7b5aaa22f0edd3565e16f6d2b2
Hamada Fadil Mahdi <hamadamahdi2014@gmail.com>
2024-10-31 09:52:07 +0000

    add main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+cat_attributes = {
+    "intelligence": 0,
+    "energy": 0,
+    "weight": 0,
+    # change the inital values above
+}
+
+print("Welcome to my cat game!")
+
+# Take the user inputs for name and colour:
+name = input("Enter name:")
+# ...
+
+# start the while loop
+while True:
+    # Finish the string below
+    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats")
+
+    if option == '1':
+        # change the cat's attributes here
+        pass
+    elif option == '2':
+        pass
+    # elif ...
+    else:
+        pass
+
+    # finish off the if statements below
+    if cat_attributes['energy'] < 0:
+        pass
+    # elif ...
+    
