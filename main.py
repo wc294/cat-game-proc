c37c683babaed24db73b046f9bfea652bf20746e
wc294 <wayne.ching@laetottenham.ac.uk>
2024-11-04 09:55:36 +0000

    hw

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 cat_attributes = {
-    "intelligence": 0,
-    "energy": 0,
-    "weight": 0,
+    "intelligence": 10,
+    "energy": 50,
+    "weight": 10,
     # change the inital values above
 }
 
@@ -17,16 +17,23 @@ while True:
     option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats")
 
     if option == '1':
-        # change the cat's attributes here
+        cat_attributes['intelligence']+=1
+        cat_attributes['energy']-=10
+        cat_attributes['weight']-=1
         pass
     elif option == '2':
+        cat_attributes['intelligence']+=10
+        cat_attributes['energy']-=10
+        cat_attributes['weight']-=10
         pass
     # elif ...
     else:
+        print (cat_attributes.values())
         pass
 
     # finish off the if statements below
     if cat_attributes['energy'] < 0:
+        endloop
+        print('dead cat')
         pass
-    # elif ...
     
